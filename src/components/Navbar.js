2b383e5c3d96e7dc106cc42e9d875536074d25d7
Divine Sackitey <divine.sackitey@hubtel.com>
2024-02-17 22:16:34 +0000

    developed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../styles/nav.css'
+import logo from '../images/synergy_tech_logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-        <h3>Jon Snow</h3>
+    <div className='nav'>
+        <img src={logo} alt='Synergy Technologies Logo' />
+        <div className='midsection'>
+            <h3>Home</h3>
+            <h3>About Us</h3>
+            <h3>Services</h3>
+            <h3>Case Studies</h3>
+            <h3>Blog</h3>
+            <h3>How it Works</h3>
+            <h3>Hire</h3>
+        </div>
+        <button><strong>Contact us</strong></button>
     </div>
   )
 }
